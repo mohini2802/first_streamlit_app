e9be7f555f9649ab145ab76ac7aad86bcf798b94
mohini2802 <132045900+mohini2802@users.noreply.github.com>
2023-05-01 02:26:29 +0530

    Update streamlit_app.py
    bring in another Python package library. This one is called requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 fruits_to_show
 
 # streamlit.dataframe(fruits_to_show)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response
