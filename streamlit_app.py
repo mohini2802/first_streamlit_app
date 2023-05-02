062a07848e1a8f4ec88ee0140bb495c88be9a3c0
mohini2802 <132045900+mohini2802@users.noreply.github.com>
2023-05-03 01:07:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_cur.execute("current_account()")
 my_data_row = my_cur.fetchone()
 streamlit.text("fruit_load_list contains:")
 streamlit.text(my_data_row)
