066cedcbbac84de75f6dd6488166eb4ecf6efb92
mohini2802 <132045900+mohini2802@users.noreply.github.com>
2023-05-18 02:29:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,30 +41,42 @@ try:
     
 except URLError as e:
     streamlit.error()
-
   
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+                                      
+                                                                                                 
+                                             
+
+                     
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_row = my_cur.fetchall()
+streamlit.text("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
 def insert_row_snowflake(new_fruit):
    my_cnx.cursor().execute("insert into pc_rivery_db.public.fruit_load_list values(new_fruit)")
    return 'Thank you for adding'+ new_fruit
                           
-def get_fruit_list():
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-        my_data_row = my_cur.fetchall()
-        streamlit.text("The fruit load list contains:")
-    return my_data_row
+if streamlit.button('Add a fruit to the list'):
+    add_fruit = streamlit.text_input('What fruit would you like to add') 
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_fruit)
+    streamlit.text(back_from_function)
 
-try:
-    if streamlit.button("View Our Fruit List - Add Your Favourites!"):
-        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-        bck_frm_fnctn_get_list = get_fruit_list()
-        streamlit.dataframe(bck_frm_fnctn_get_list)
-        add_fruit = streamlit.text_input('What fruit would you like to add')
-        streamlit.write('Thank you for adding ', add_fruit)
-        streamlit.text(add_fruit)
-        back_from_function = insert_row_snowflake("jackfruit")
-        # streamlit.text(back_from_function)
     
-except URLError as e:
-    streamlit.error()
+streamlit.header("View Our Fruit List - Add Your Favourites!")
+if streamlit.button("Get Fruit List"):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("delete from pc_rivery_db.public.fruit_load_list where fruit_name like 'test' or fruit_name ='from streamlit'")
+      my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+      my_data_row = my_cur.fetchall()
+                                                                                   
+             
+                                                              
+                                                   
+      streamlit.dataframe(my_data_row)
+
+                     
+                     
 
