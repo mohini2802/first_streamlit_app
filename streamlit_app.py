8f958df9864b65459cdd7717060d36bd931cebea
mohini2802 <132045900+mohini2802@users.noreply.github.com>
2023-05-18 00:28:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ def insert_row_snowflake(add_fruit):
    return 'Thank you for adding'+ add_fruit
 
 def get_fruit_list():
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_cur = my_cnx.cursor()
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     my_data_row = my_cur.fetchall()
